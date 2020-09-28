76799e7151a89f9fc4fe3ed896cde47dd1804e24
xaviguasch <xaviguasch@gmail.com>
2020-09-28 15:30:58 +0200

    added checkboxes in the App file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,14 @@ import './App.css'
 function App() {
   return (
     <div className='App'>
-      <h2>Hey there!</h2>
+      <input type='checkbox' name='' id='' />
+      <p>Item one</p>
+      <input type='checkbox' name='' id='' />
+      <p>Item two</p>
+      <input type='checkbox' name='' id='' />
+      <p>Item three</p>
+      <input type='checkbox' name='' id='' />
+      <p>Item four</p>
     </div>
   )
 }
