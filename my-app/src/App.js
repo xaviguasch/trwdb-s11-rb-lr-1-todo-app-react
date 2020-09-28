0662a8917844ff652f6b9065411e2d35ebe71b3f
xaviguasch <xaviguasch@gmail.com>
2020-09-28 20:15:25 +0200

    separated todo items into its own components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import './App.css'
 
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import TodoItem from './components/TodoItem'
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
+    <div className='todo-list'>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   )
 }
--- my-app/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Footer() {
-  return <footer>This is the footer</footer>
-}
-
-export default Footer
