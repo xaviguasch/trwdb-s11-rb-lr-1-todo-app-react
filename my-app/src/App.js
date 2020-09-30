f28afa787b3df724636898ebace3a2197a283aa4
xaviguasch <xaviguasch@gmail.com>
2020-09-30 12:24:54 +0200

    added new todo items through mapping

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,12 @@ import React from 'react'
 import './App.css'
 
 import TodoItem from './components/TodoItem'
+import todosData from './todosData'
 
 function App() {
-  return (
-    <div className='todo-list'>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  )
+  const todoItems = todosData.map((item) => <TodoItem key={item.id} item={item} />)
+
+  return <div className='todo-list'>{todoItems}</div>
 }
 
 export default App
