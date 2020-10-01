91c570d1eefd7780db19b4cda11c6ebd01e83172
xaviguasch <xaviguasch@gmail.com>
2020-10-01 21:43:35 +0200

    modified completed state of each todo via handleChange function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,3 @@
-/*
-In the previous iteration of this todo list app, we pulled in todos data from a JSON file and mapped over it to display the todo items.
-
-Eventually we'll want to be able to modify the data, which will only happen if we've "loaded" the data in to the component's state
-
-Challenge: Change the <App /> component into a stateful class component and load the imported `todosData` into state.
-*/
-
 import React from 'react'
 import TodoItem from './components/TodoItem'
 import todosData from './todosData'
@@ -16,11 +8,30 @@ class App extends React.Component {
     this.state = {
       todos: todosData,
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      })
+
+      return {
+        todos: updatedTodos,
+      }
+    })
   }
 
   render() {
     const todoItems = this.state.todos.map((item) => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ))
 
     return <div className='todo-list'>{todoItems}</div>
